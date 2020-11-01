dd06745b6de400ecb25e37584f88058d6f490af9
pnpjss <nordeborg.l@gmail.com>
2020-11-01 21:54:00 +0100

    Code clean up

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php
 
 declare(strict_types=1);
-function sortFunction($a, $b)
+
+function sortFunction($a, $b): int
 {
     return strtotime($a["published"]) - strtotime($b["published"]);
 }
 
-// This is the file where you can keep all your functions. Remember to NOT
-// execute/run any functions in this file. Keep it dumb.
+usort($articles, "sortFunction");
+$articles = array_reverse($articles);
