a302eab5095e78611671d7fdc8d381f8ea380dc4
pnpjss <nordeborg.l@gmail.com>
2020-11-01 16:41:17 +0100

    Added declare strict types

--- index.php
+++ index.php
@@ -1,25 +1,14 @@
-<?php require('header.php') ?>
+<?php
+
+declare(strict_types=1);
+
+require('header.php') ?>
 <!-- // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether. -->
 
 <body>
 
-    <div class="grid-container"></div> <!-- Grid container for news items -->
-    <?php require('data.php'); ?>
-    <!-- Fetch articles array -->
-
-    <?php foreach ($article as $art) : ?>
-        <div class="grid-item">
-            <h1> <?php echo $art['title']; ?> </h1>
-            <p class="p-content"> <?php echo $art['content']; ?></p>
-
-            <?php echo '- ', $art['author']; ?><br>
-
-            <?php echo ' ' . $art['publishedDate']; ?>
-            <?php echo ' ' . $art['likes'] + rand(0, 10000) . '❤'; ?>
-            </p>
-        </div>
-    <?php endforeach; ?>
-
+    <?php require('articles.php'); ?>
+    <div class="footer"></div>
     <?php require('footer.php'); ?>
