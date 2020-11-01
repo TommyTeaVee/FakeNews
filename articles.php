e736d4e08473e9e0f603e77fa488dcbc67bbb4aa
pnpjss <nordeborg.l@gmail.com>
2020-11-01 19:04:38 +0100

    Test update

--- articles.php
+++ articles.php
@@ -2,20 +2,16 @@
 
 declare(strict_types=1); ?>
 
-<div class="grid-container"></div> <!-- Grid container for news items -->
+<main class="grid-container"></main>
 <?php require('data.php'); ?>
-<!-- Fetch articles array -->
-
 <?php foreach ($articles as $article) : ?>
     <div class="grid-item">
         <h1> <?php echo $article['title']; ?> </h1>
         <br>
-        <p class="p-content"> <?php echo $article['content']; ?></p> <br>
+        <p class="p-content"> <?php echo $article['content']; ?> </p><br>
         <p class="p-author"><?php echo '- ', $article['author']; ?> </p>
         <?php echo ' ' . $article['published']; ?>
         <?php echo ' ' . $article['likes'] + rand(0, 10000) . ' ❤'; ?>
-
-
     </div>
-    <span class="articleEnd"> </span>
+    <span class="article-end"> </span>
 <?php endforeach; ?>
